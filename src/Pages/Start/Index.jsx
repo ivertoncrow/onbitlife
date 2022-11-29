652ba0ef68fd3ef75b221e90371c041783ce7a48
ivertoncrow <ivertonmello@gmail.com>
2022-11-29 12:01:30 -0300

    Estrutura + Estilização(corrigidas)

--- src/Pages/Start/Index.jsx
+++ src/Pages/Start/Index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ScrollView, View, Image, Text, StyleSheet } from "react-native";
 
+import LifeStatus from "../../Components/Common/LifeStatus";
 export default function Start() {
   return (
     <View style={styles.conteiner}>
@@ -10,6 +11,7 @@ export default function Start() {
             source={require("../../assets/icons/logo3.png")}
             style={styles.logo}
           />
+          <LifeStatus></LifeStatus>
           <Text style={styles.description}>
             Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o
             melhor nível.
