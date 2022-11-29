17f34c551f3be3a34534874a5ac35d6316efa2f3
ivertoncrow <ivertonmello@gmail.com>
2022-11-29 02:27:03 -0300

    Estilização da página Start(+correções)

--- src/Pages/Start/Index.jsx
+++ src/Pages/Start/Index.jsx
@@ -1,19 +1,40 @@
 import React from "react";
-import { ScrollView, View , Image , Text } from "react-native";
+import { ScrollView, View, Image, Text, StyleSheet } from "react-native";
 
 export default function Start() {
-return(
-  <View>
-    <ScrollView showsVerticalScrollIndicator={false}>
-      <View>
-        <Image source={require("../../assets/icons/logo3.png")}/>
-        <Text>
-          Vamos transformar sua vida {/n} em jogo, buscando sempre {/n} o 
-          melhor nível.
-        </Text>
-      </View>
-    </ScrollView>
-  </View>
-)
-
+  return (
+    <View style={styles.conteiner}>
+      <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+        <View style={{ alignItems: "center" }}>
+          <Image
+            source={require("../../assets/icons/logo3.png")}
+            style={styles.logo}
+          />
+          <Text style={styles.description}>
+            Vamos transformar sua vida {"\n"} em jogo, buscando sempre {"\n"} o
+            melhor nível.
+          </Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  conteiner: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBotoom: 20,
+  },
+  description: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 60,
+  },
+});
